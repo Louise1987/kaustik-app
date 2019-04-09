95f1203a72ed16403f69526d1e214597a9abccda
LouiseThomsson <louise.thomsson@netrelations.com>
2019-04-09 17:05:49 +0200

    Firebase & frontend
    Kopplat databas och gjort mycket av frontend delen.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,75 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
+	
+		constructor(props) {
+			super(props);
+			this.state = {
+				shown: true,
+				items: [],
+				isLoaded: false,
+			}
+		}
+
+		componentDidMount(){
+			fetch('http://www.mocky.io/v2/5c9cdca03300004d003f2151')
+			.then(res => res.json())
+			.then(json => {
+				this.setState({
+					isLoaded:true,
+					items: json,
+				})
+			});
+		}
+
+		toggle() {
+			this.setState({
+				shown: !this.state.shown
+			});
+		}
+
   render() {
+			var shown = {
+				display: this.state.shown ? "block" : "none"
+			};
+
+			var hidden = {
+				display: this.state.shown ? "none" : "block"
+			}
+
+		var { isLoaded, items } = this.state;
+
+		if (!isLoaded){
+			return <div>Loading...</div>;
+		}
+		else {
+
     return (
       <div className="App">
-        <header className="App-header">
-					<p>
-						Skriv ditt schema
-					</p>
-					<div class="input-block">
-						<input type="text" placeholder="Aktivitet" />
-							<label for="meeting">Välj start tid</label>
-						<input type="datetime-local" id="meeting-time"
-       				name="meeting-time" placeholder="Starttid" />
-							<label for="meeting">Välj slut tid</label>
-						<input type="datetime-local" id="meeting-time"
-       				name="meeting-time" placeholder="Sluttid" />
-						<input type="text" placeholder="Plats" />
-					</div>
-					<div class="block-button"><button onClick="Add">Spara</button></div>
-        </header>
+				<div className="AppHeader">
+					<h1>Boka ditt möte nedan</h1>
+				</div>
+        <ul>
+					{items.map(item => (
+						<li key={item.id} style={shown}> 
+							<button className="select" style={ shown }> 
+							{item.activity}<br/>
+							Starttid: {item.startDate}<br/>
+							Sluttid: {item.endDate}<br/> 
+							Plats: {item.location}<br/>
+						</button>
+						</li>
+					))}
+					</ul>
+				{/* visas inte innan möte valts */}
+					<button className="saveBooking" style={hidden}></button> 
       </div>
     );
-  }
+	}
 }
+}
+
 
 export default App;
---
+++ src/fire.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.9.3/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyALbIk_xpaFuqp5CcAaiAGm3orepNx17xA",
+    authDomain: "kaustik-643f6.firebaseapp.com",
+    databaseURL: "https://kaustik-643f6.firebaseio.com",
+    projectId: "kaustik-643f6",
+    storageBucket: "kaustik-643f6.appspot.com",
+    messagingSenderId: "61029332159"
+  };
+  firebase.initializeApp(config);
+</script>
