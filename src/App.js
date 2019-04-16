e3ddb02837cdd21c4954deec558dd958305c4d58
LouiseThomsson <louise.thomsson@netrelations.com>
2019-04-16 15:08:19 +0200

    Firebase
    databas kopplad

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import firebase from 'firebase';
+const uuid = require('uuid');
+
+
+// firebase.auth().currentUser.getIdToken()
+//   .then((idToken) => {
+//     // idToken can be passed back to server.
+//   })
+//   .catch((error) => {
+//     // Error occurred.
+//   });
 
 class App extends Component {
 	
 		constructor(props) {
 			super(props);
+			// gör strängar av state
+			this.state = {
+				uid: uuid.v1(),
+				meeting:'',
+				name:'',
+				email:'',
+			};
+			
+			// binder propertys till click funktion 
+			this.handleClick = this.handleClick.bind(this);
+			this.handleChange = this.handleChange.bind(this);
+			this.handleSubmit = this.handleSubmit.bind(this);
+			this.inputData = this.inputData.bind(this);
 
+			// kopplar databas
 			var config = {
 				apiKey: "AIzaSyALbIk_xpaFuqp5CcAaiAGm3orepNx17xA",
 				authDomain: "kaustik-643f6.firebaseapp.com",
@@ -16,25 +40,29 @@ class App extends Component {
 				messagingSenderId: "61029332159"
 			};
 			firebase.initializeApp(config);
-			console.log(firebase);
+			// console.log(firebase);
 
+			// Instans för att skriva data till och från databas
+			// skapar databasen lokalt med referens ref
 			var database = firebase.database();
 			var ref = database.ref('meeting');
 
+			// variabel data med sträng id
 			var data ={
-				name: "LTE",
-				meeting: 1
+				id: '',
 			}
+			// Pushar upp data till databas
 			ref.push(data);
-
-			this.state = {
-				items: [],
-				isLoaded: false,
-
-			}
 		}
+		
+		// hämtar klick för mötes knappar och skriver ut text i knappar
+		handleClick = (e) =>{
+			console.log(e.target.innerHTML);
+			alert('Du har valt ett möte');
+		}
+
+		// hämtar API för olika möten 
 		componentDidMount(){
-			
 			fetch('http://www.mocky.io/v2/5c9cdca03300004d003f2151')
 			.then(res => res.json())
 			.then(json => {
@@ -53,11 +81,14 @@ class App extends Component {
 						}
 				
 						meetings.push(day1,day2)
+
+				
 				
 					}else{
 						meetings.push(meeting)
 					}
-					
+			
+
 				});
 				console.log(meetings)
 				this.setState({
@@ -65,9 +96,45 @@ class App extends Component {
 					items: meetings,
 				})
 			});
+
+			// import firebase och ref sträng
+			firebase
+			.database()
+			.ref(`Newdata/${this.state.uid}`)
+			.on('value', snap => console.log('from db', snap.val()));
 		}
 
-	
+
+		// hämtar ny data 
+		handleChange(e){
+		this.setState({
+			name: e.target.name});
+			
+	}
+
+	// hämtar ref och skriver ut sträng med set till firebase
+	handleSubmit(e){
+		alert('Er bokning är bekräftad: ' + this.state.value);
+		console.log('Du har bekräftat er bokning')
+		e.preventDefault();
+		firebase
+		.database()
+		.ref(`Newdata/${this.state.uid}`)
+		.set({
+			meeting: this.state.meeting,
+			name: this.state.name,
+			email: this.state.email,
+		})
+		.catch(error => console.log(error));
+	}
+
+// knyter input text till property
+	inputData (_e){
+		const meeting = this.refs.meeting1.value;
+		const name = this.refs.name1.value;
+		const email = this.refs.email1.value;
+		this.setState({ meeting, name, email});
+	}
 
   render() {
 
@@ -86,10 +153,12 @@ class App extends Component {
 					
 				</div>
         <ul>
-							
+				
 					{items.map((item,i) => (
 						<li key={i}>
-							<button className="select">
+
+					{/* kopplar handleClick till onChange*/}
+							<button onClick={(e) => this.handleClick(e)} onChange={this.inputData} ref="meeting1" className="select">
 							{item.activity}<br/>
 							Starttid: {item.startDate}<br/>
 							Sluttid: {item.endDate}<br/> 
@@ -101,19 +170,19 @@ class App extends Component {
 					</ul>
 				
       </div>
-		
-			}
-			
+
 		<div className="selectedMeeting">Fyll i dina uppgifter och bekräfta</div>
 			
-				<form className="bookingSection">
+				<form onSubmit={this.handleSubmit} className="bookingSection">
 					<label>
 						Name:
-						<input type="text" name="name" />
+						<input type="text" value={this.state.name} onChange={this.inputData}
+						ref="name1"/>
 					</label>
 					<label>
 						E-mail:
-						<input type="text" email="email" />
+						<input type="text" value={this.state.email} onChange={this.inputData}
+						ref="email1"/>
 					</label>
 					<input className="confirm" type="submit" value="Bekräfta" />
 				</form>
